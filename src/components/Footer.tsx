307263c43eca1eeffc16ea6ab3f2527114536e00
kajang93 <stars_ka@naver.com>
2025-11-26 23:10:48 +0900

    Introduce new UI component library

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer: React.FC = () => {
-    return (
-        <footer style={{ textAlign: "center", padding: "20px", color: "#888" }}>
-            © 2025 Kuji App
-        </footer>
-    );
-};
